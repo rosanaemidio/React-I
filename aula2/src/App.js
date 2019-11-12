24b22a178acb0b853f881deee346e069ee021e03
rosanaemidio <rosanaemidio42@gmail.com>
2019-11-12 16:01:34 -0300

    Adiciona parte do exercicio previsão do tempo e contador

--- aula2/src/App.js
+++ aula2/src/App.js
@@ -1,47 +1,32 @@
 import React from 'react';
 import Solzin from './img/sol.png';
 import Nuvenzin from './img/nuvens.png';
+import CriaCards from './componentes/CriaCards'
 import logo from './logo.svg';
 import './App.css';
 
-const CriaCards = props => {
-    return (
-      <div className="previsao">
-        <h3 className="previsao-data">{props.data}</h3>
-        <p className="previsao-resumo">{props.resumo}</p>
-          <img className="previsao__img" src={props.img}/>
-        <table>
-          <tr className="previsao-temperatura">
-            <td className="previsao-temperatura__linha">Máxima</td>
-            <td className="previsao-temperatura__linha">{props.max} C</td>
-          </tr>
-          <tr className="previsao-temperatura">
-            <td className="previsao-temperatura__linha">Mínima</td>
-            <td className="previsao-temperatura__linha">{props.min} C</td>
-          </tr>
-        </table>
-      </div>
-    )
-
-}
 
 function App() {
   return (
     <div className="previsao-container">
-      <div className="previsao">
-        <CriaCards
-          data = "25/08/2020"
-          resumo = "Ensolarado"
-          img = {Solzin}
-          max = {32}
-          min = {21}
-
-        
-        />
-
-
-      </div>
-      
+          <CriaCards
+            data = "25/08/2020"
+            resumo = "Ensolarado"
+            img = {Solzin}
+            temperatura ={{
+              max : 32,
+              min : 21
+            }}       
+          />
+          <CriaCards
+            data="01/06/2019"
+            resumo="Nublado"
+            img={Nuvenzin}
+            temperatura={{
+              max: 25,
+              min: 18
+            }}
+          />     
     </div>
   );
 }
--- contador-react/first-contador/src/App.js
+++ contador-react/first-contador/src/App.js
@@ -3,22 +3,53 @@ import logo from './logo.svg';
 import './App.css';
 
 class Contador extends React.Component{
+  constructor (props) {
+    super(props);
+    this.state = {
+      contador:0
+    };
+  }
 
-  
+  adicionaUm =() => {
+    this.setState((prevState) => {
+      return {
+        contador: prevState.contador + 1
+      }
+    })
+  }
+
+  removeUm = () => {
+    this.setState((prevState)=>{
+      return{
+        contador: prevState.contador - 1
+      }
+    })
+  }
+
+  render(){
+    return (
+      <div>
+          <p>{this.state.contador}</p>
+           <button className="BtnSoma" onClick={this.adicionarUm}>+1</button>
+           <button className="BtnSubtrai" onClick={this.removeUm}>-1</button>
+            <button className="LimpaTudo">Resetar</button>
+
+      </div>
+
+
+    )
+
+
+
+  }
 }
 
 
 function App() {
   return (
     <div className="App">
-          <h1>Contador: </h1>
-  <p>{Contador}</p>
-      {verificaContador()}
-      <button className="BtnSoma">+1</button>
-      <button className="BtnSubtrai">-1</button>
-      <button className="LimpaTudo">Resetar</button>
-
-    
+          <h2>Contador</h2>
+    <Contador />  
     </div>
   );
 }
